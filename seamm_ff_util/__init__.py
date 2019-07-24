ff95877da045f12fb00f6e2d941a780def62e10e
Paul Saxe <paul@thesaxes.net>
2019-07-24 15:30:14 -0400

    Switching to versioneer

--- seamm_ff_util/__init__.py
+++ seamm_ff_util/__init__.py
@@ -7,3 +7,10 @@ __version__ = '0.2.1'
 
 from seamm_ff_util.forcefield import Forcefield  # noqa: F401
 from seamm_ff_util.ff_assigner import FFAssigner  # noqa: F401
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
