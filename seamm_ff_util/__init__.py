d08d2637dbdf2b8a1731052ebaa0487c33ca3c28
Paul Saxe <paul@thesaxes.net>
2023-02-06 16:51:04 -0500

    Made relative paths...

--- seamm_ff_util/__init__.py
+++ seamm_ff_util/__init__.py
@@ -6,8 +6,8 @@ __author__ = """Paul Saxe"""
 __email__ = "psaxe@molssi.org"
 __version__ = "0.2.1"
 
-from seamm_ff_util.forcefield import Forcefield  # noqa: F401
-from seamm_ff_util.ff_assigner import FFAssigner  # noqa: F401
+from .forcefield import Forcefield  # noqa: F401
+from .ff_assigner import FFAssigner  # noqa: F401
 
 # Handle versioneer
 from ._version import get_versions
